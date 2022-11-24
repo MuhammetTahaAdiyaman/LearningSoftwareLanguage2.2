7043fde0b7e1382633dd371796deca9562351ea0
Muhammet Taha Adıyaman <68828015+MuhammetTahaAdiyaman@users.noreply.github.com>
2022-11-24 23:07:23 +0300

    Add files via upload

---
+++ DERS4/DERS4/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DERS4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //kullanıcıdan ismini, yaşını ve boy bilgisini ekrana bastıran bir program yazalım.
+
+            Console.WriteLine("************ Welcome To The System ************");
+            string Name;
+            string Surname;
+            int Age;
+            float Weight;
+
+            Console.WriteLine("Please enter the name");
+            Name = Console.ReadLine();
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Please enter the surname");
+            Surname = Console.ReadLine();
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Please enter the age");
+            Age = int.Parse(Console.ReadLine());
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Please enter the weight");
+            Weight = (float)Convert.ToDouble(Console.ReadLine());
+            //Weihht = float.parse(Console.WriteLine());
+            Console.WriteLine("\n");
+            Console.WriteLine("************ Print To The System ************");
+            Console.WriteLine("-------------------------------------------");
+            Console.WriteLine("NAME: " + Name);
+            Console.WriteLine("SURNAME: " + Surname);
+            Console.WriteLine("AGE: " + Age);
+            Console.WriteLine("WEIGHT: " + Weight);
+            Console.WriteLine("-------------------------------------------");
+            Console.ReadLine(); //pencereyi ekranda tutmak için yazdığım bir kod 
+
+
+
+
+        }
+    }
+}
---
+++ DERS5/DERS5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DERS5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Operators of Math
+            int sayi1 = 30;
+            int sayi2 = 20;
+
+            Console.WriteLine(sayi1 + sayi2);
+            Console.WriteLine(sayi1 - sayi2);
+            Console.WriteLine(sayi1 * sayi2);
+            //Console.WriteLine(sayi1 / sayi2);
+            Console.WriteLine(((double)sayi1 / (double)sayi2));
+            Console.WriteLine(sayi1 % sayi2);
+
+
+
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
