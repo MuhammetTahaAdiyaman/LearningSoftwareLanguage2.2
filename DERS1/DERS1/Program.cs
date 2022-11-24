41276b062a9e46a58ecf0afc2486b58c5e2b038c
Muhammet Taha Adıyaman <68828015+MuhammetTahaAdiyaman@users.noreply.github.com>
2022-11-24 23:04:51 +0300

    Add files via upload

---
+++ DERS1/DERS1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DERS1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello world");
+
+            /*bu method normalde kullanıcının klavyeden girdiği değeri okuması için. 
+            Ancak burada kullanılma amacı hello world yazısını ekranda kalmasını sağlamak içindir.*/
+            Console.ReadLine();  
+            
+
+        }
+    }
+}
